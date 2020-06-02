475285762ffbbe1eb050cc0682c25d9c04b52484
gsfellis <gregfellis@gmail.com>
2020-06-02 08:18:02 -0400

    Implement Goodreads API

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
 
 function goodreadsService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'our description' });
+      axios.get(`https://www.goodreads.com/book/show/${id}.xml?key=sNExSSlviJzbzI7e9jMg`)
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
 
